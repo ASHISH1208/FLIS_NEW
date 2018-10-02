09db8c9cf70286173ee6f149056b67f3b6b56681
ASHISH KUMAR <krashish1208@gmail.com>
2018-10-02 19:13:30 +0530

    Update user_request.rb

--- app/models/user_request.rb
+++ app/models/user_request.rb
@@ -1,6 +1,8 @@
 class UserRequest < ApplicationRecord
 	validates :name,:village,:address,:loss_info,:loss_amount, presence: true
 	validates :contact_no,length: { is: 10 }, uniqueness: true
+	VALID_LOSS_AMOUNT_REGEX=/[^0]/
+	validates :loss_amount, presence: true, format: { with: VALID_LOSS_AMOUNT_REGEX }
         VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
         validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
